bfddb09c992d1d02fafbe086ad68caf50e00adb8
WatsonJoev <81696671+WatsonJoev@users.noreply.github.com>
2024-10-28 21:14:14 +0530

    Initial Commit
    Initial Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// onUserClick - Camel case
+
+
+// let, var, const = Variables -- Declar variable
+
+console.log("External Javascript is loaded")
+
+// window - Browser windows - alerts, confirm and navigation
+// document - Whatever renders in browser will be considered as document
+// console - f12 or inscept to a webpage
+
+function onUserClick(){
+    console.log("User click on button")
+    document.getElementById("bobTag").innerHTML = "<h3>Hey it's an h3</h3>"
+    document.getElementById("bobTag").innerText = "Tom raiding bike"
+}
